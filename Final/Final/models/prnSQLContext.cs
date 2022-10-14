7e49246a28864d8876396f2d139978303dc0482e
HuyTQ-FPT <105337431+HuyTQ-FPT@users.noreply.github.com>
2022-10-14 14:04:24 +0700

    oke

---
+++ Final/Final/models/prnSQLContext.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Final.models
+{
+    public partial class prnSQLContext : DbContext
+    {
+        public prnSQLContext()
+        {
+        }
+
+        public prnSQLContext(DbContextOptions<prnSQLContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Bill> Bills { get; set; }
+        public virtual DbSet<BillDetail> BillDetails { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Original> Originals { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=Localhost;database=prnSQL;user=sa;password=123456");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.Username)
+                    .HasName("PK__Account__536C85E57F60ED59");
+
+                entity.ToTable("Account");
+
+                entity.Property(e => e.Username).HasMaxLength(50);
+
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.Property(e => e.Phone).IsRequired();
+            });
+
+            modelBuilder.Entity<Bill>(entity =>
+            {
+                entity.ToTable("Bill");
+
+                entity.Property(e => e.BillId).HasColumnName("BillID");
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.UsernameNavigation)
+                    .WithMany(p => p.Bills)
+                    .HasForeignKey(d => d.Username)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Bill__Username__0519C6AF");
+            });
+
+            modelBuilder.Entity<BillDetail>(entity =>
+            {
+                entity.HasKey(e => new { e.BillId, e.ProductId })
+                    .HasName("bill_pk");
+
+                entity.ToTable("BillDetail");
+
+                entity.Property(e => e.BillId).HasColumnName("BillID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.HasOne(d => d.Bill)
+                    .WithMany(p => p.BillDetails)
+                    .HasForeignKey(d => d.BillId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BillDetai__BillI__164452B1");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.BillDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BillDetai__Produ__15502E78");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e => e.CateId)
+                    .HasName("PK__Category__27638D740BC6C43E");
+
+                entity.ToTable("Category");
+
+                entity.Property(e => e.CateId).HasColumnName("CateID");
+
+                entity.Property(e => e.OriginalId).HasColumnName("OriginalID");
+
+                entity.HasOne(d => d.Original)
+                    .WithMany(p => p.Categories)
+                    .HasForeignKey(d => d.OriginalId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Category__Origin__0DAF0CB0");
+            });
+
+            modelBuilder.Entity<Original>(entity =>
+            {
+                entity.ToTable("Original");
+
+                entity.Property(e => e.OriginalId).HasColumnName("OriginalID");
+
+                entity.Property(e => e.OriginalName).HasColumnName("OriginalName");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.CateId).HasColumnName("CateID");
+
+                entity.Property(e => e.ProductName).IsRequired();
+
+                entity.HasOne(d => d.Cate)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CateId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Product__CateID__1273C1CD");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
