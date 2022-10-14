a39372cb2c4324097a90a34171883f1489e3ab86
HuyTQ-FPT <105337431+HuyTQ-FPT@users.noreply.github.com>
2022-10-14 13:56:06 +0700

    uo file

---
+++ Final/Final/models/BillDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Final.models
+{
+    public partial class BillDetail
+    {
+        public int ProductId { get; set; }
+        public int BillId { get; set; }
+        public int QuantityUnit { get; set; }
+        public int Tax { get; set; }
+
+        public int Price { get { return Tax * QuantityUnit; } }
+        public virtual Bill Bill { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
